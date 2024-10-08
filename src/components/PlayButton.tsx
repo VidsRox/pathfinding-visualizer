a3f406efb3c1ac7aac76ffa7433bade00e7df1d9
Vidyun Agarwal <vidyunagarwal@gmail.com>
2024-10-08 14:46:50 +0530

    navigation cmpnt outline

---
+++ src/components/PlayButton.tsx
@@ -0,0 +1,16 @@
+import { MouseEventHandler } from "react";
+
+export function PlayButton({
+    handlerRunVisualizer,
+    isDisabled,
+    isGraphVisualized,
+}: 
+ {
+    isDisabled: boolean;
+    isGraphVisualized: boolean;
+    handlerRunVisualizer: MouseEventHandler<HTMLButtonElement>;
+ }) {
+    return (
+        
+    )
+ }
